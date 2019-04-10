d06db744795deec6e9460514b573c93ff1f2c3c1
SoYouDie <roman.bv20@gmail.com>
2019-04-10 17:00:19 +0300

    Fix stock algorithm and add jshint.rc

--- controllers/best-time-to-buy-and-sell-stock.js
+++ controllers/best-time-to-buy-and-sell-stock.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let priceToBuy = priceToSell = 0;
-    let dayToSell = 0;
+    let profit = 0;
     prices.forEach((price, index) => {
-        if (!priceToBuy || price < priceToBuy) {
-            priceToBuy = priceToSell = price;
-            dayToBuy = index;
-        }
-        if (price > priceToSell && index) {
-            priceToSell = price;
+        for (let i = index; i < prices.length; i++) {
+            let priceDiff = prices[i] - price;
+            if (priceDiff > profit) profit = priceDiff;
         }
     });
-    return priceToSell - priceToBuy > 0 ? priceToSell - priceToBuy : 0;
+    return profit;
 };
 
 module.exports = maxProfit;
--- tests/best-time-to-buy-and-sell-stock.test.js
+++ tests/best-time-to-buy-and-sell-stock.test.js
@@ -3,5 +3,5 @@ const maxProfit = require('../controllers/best-time-to-buy-and-sell-stock');
 const stock1 = [7, 1, 5, 3, 6, 4];
 const stock2 = [2, 4, 1];
 
-// console.log(maxProfit(stock1));
+console.log(maxProfit(stock1));
 console.log(maxProfit(stock2));
