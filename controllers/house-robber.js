6cf667d238406717ba8a26a0ccaa26bdaf6eec6f
SoYouDie <roman.bv20@gmail.com>
2019-05-03 14:54:22 +0300

    Improve house-robber algorithm

--- controllers/house-robber.js
+++ controllers/house-robber.js
@@ -2,39 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    let startResult = 0;
-    let endResult = 0;
-    let lastIndex = nums.length;
-    for (let i = 0, j = lastIndex - 1; i < nums.length; i += 2, j -= 2) {
-        if (
-            i + 1 < lastIndex &&
-            (
-                i + 3 < lastIndex && nums[i + 1] + nums[i + 3] > nums[i] + nums[i + 2] ||
-                i + 2 < lastIndex && nums[i + 1] >= nums[i] + nums[i + 2] ||
-                i + 2 >= lastIndex && nums[i + 1] >= nums[i]
-            )
-         ) {
-            startResult += nums[i + 1];
-            i++;
-        } else {
-            startResult += nums[i];
-        }
-        if (
-            j - 1 > 0 &&
-            (
-                i + 3 >= 0 && nums[i - 1] + nums[i - 3] > nums[i] + nums[i - 2] ||
-                j - 2 >= 0 && nums[j - 1] >= nums[j] + nums[j - 2] ||
-                j - 2 < 0 && nums[j - 1] >= nums[j]
-            )
-         ) {
-            endResult += nums[j - 1];
-            j--;
-        } else {
-            endResult += nums[j];
-        }
-    }
-    return endResult > startResult ? endResult : startResult;
+var rob = function(nums) {    
+    return nums.reduce(function(sumArr, value) {
+        return [sumArr[1], Math.max(sumArr[0] + value, sumArr[1])]; 
+    }, [0,0])[1];
 };
 
 module.exports = rob;
--- tests/house-robber.test.js
+++ tests/house-robber.test.js
@@ -1,7 +1,9 @@
 const rob = require('../controllers/house-robber');
 
-const arr1 = [4, 1, 2, 7, 5, 3, 1];
-const arr2 = [2, 7, 7, 7, 4];
+const arr1 = [1, 1, 3, 6, 7, 10, 7, 1, 8, 5, 9, 1, 4, 4, 3];
+const arr2 = [2, 1, 1, 2];
+const arr3 = [2, 4, 8, 9, 9, 3];
 
 console.log(rob(arr1));
-// console.log(rob(arr2));
+console.log(rob(arr2));
+console.log(rob(arr3));
